de0449aaad8e81cd6869e1ca536cccb14ab1bc4a
myrzabekk <Gapusta97@gmail.com>
2023-10-27 18:18:28 +0600

    Added new tests

--- src/main/kotlin/edu/myrza/archke/util/ByteUtils.kt
+++ src/main/kotlin/edu/myrza/archke/util/ByteUtils.kt
@@ -14,3 +14,17 @@ fun ByteBuffer.getPositiveInt(offset: Int): Int {
             (this[offset + 2].toInt() shl 8) or
             (this[offset + 3].toInt())
 }
+
+/**
+ *  Returns byte representation of 32-bit Integer in big-endian byte order (most significant bytes come first)
+ *  min value is 0
+ *  max value is 2097152
+ */
+fun Int.getBytes(): ByteArray {
+    val array = ByteArray(4)
+    array[0] = (this shr 24).toByte()
+    array[1] = (this shr 16).toByte()
+    array[2] = (this shr 8).toByte()
+    array[3] = this.toByte()
+    return array
+}
--- src/test/kotlin/edu/myrza/archke/util/ByteUtilsTest.kt
+++ src/test/kotlin/edu/myrza/archke/util/ByteUtilsTest.kt
@@ -7,7 +7,7 @@ import kotlin.test.assertEquals
 class ByteUtilsTest {
 
     @Test
-    fun testConvert() {
+    fun testBytesToInt() {
         val buffer = ByteBuffer.allocate(4).apply { this.putInt(69) }
 
         val result = buffer.getPositiveInt(0)
@@ -16,7 +16,7 @@ class ByteUtilsTest {
     }
 
     @Test
-    fun testConvertMinValue() {
+    fun testBytesToIntMinValue() {
         val buffer = ByteBuffer.allocate(4).apply { this.putInt(0) }
 
         val result = buffer.getPositiveInt(0)
@@ -25,7 +25,7 @@ class ByteUtilsTest {
     }
 
     @Test
-    fun testConvertMaxValue() {
+    fun testBytesToIntMaxValue() {
         val buffer = ByteBuffer.allocate(4).apply { this.putInt(2097152) }
 
         val result = buffer.getPositiveInt(0)
@@ -34,7 +34,7 @@ class ByteUtilsTest {
     }
 
     @Test
-    fun testConvertWithOffset() {
+    fun testBytesToIntWithOffset() {
         val buffer = ByteBuffer.allocate(5).apply {
             this.put(69)
             this.putInt(3142)
@@ -46,7 +46,7 @@ class ByteUtilsTest {
     }
 
     @Test
-    fun testConvertMinValueWithOffset() {
+    fun testBytesToIntMinValueWithOffset() {
         val buffer = ByteBuffer.allocate(5).apply {
             this.put(69)
             this.putInt(0)
@@ -58,7 +58,7 @@ class ByteUtilsTest {
     }
 
     @Test
-    fun testConvertMaxValueWithOffset() {
+    fun testBytesToIntMaxValueWithOffset() {
         val buffer = ByteBuffer.allocate(5).apply {
             this.put(69)
             this.putInt(2097152)
@@ -67,6 +67,39 @@ class ByteUtilsTest {
         val result = buffer.getPositiveInt(1)
 
         assertEquals(2097152, result)
+    }
+
+    @Test
+    fun testIntToBytes() {
+        val expected = byteArrayOf(0, 0, 16, 115)
+        val result = 4211.getBytes()
+
+        assertEquals(expected[0], result[0])
+        assertEquals(expected[1], result[1])
+        assertEquals(expected[2], result[2])
+        assertEquals(expected[3], result[3])
+    }
+
+    @Test
+    fun testIntToBytesMinValue() {
+        val expected = byteArrayOf(0, 0, 0, 0)
+        val result = 0.getBytes()
+
+        assertEquals(expected[0], result[0])
+        assertEquals(expected[1], result[1])
+        assertEquals(expected[2], result[2])
+        assertEquals(expected[3], result[3])
+    }
+
+    @Test
+    fun testIntToBytesMaxValue() {
+        val expected = byteArrayOf(0, 32, 0, 0)
+        val result = 2097152.getBytes()
+
+        assertEquals(expected[0], result[0])
+        assertEquals(expected[1], result[1])
+        assertEquals(expected[2], result[2])
+        assertEquals(expected[3], result[3])
     }
 
 }

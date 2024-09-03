53b4890366beb05670f1ff66e3c5ac68c0178c88
gapusta <gapusta97@gmail.com>
2024-09-03 20:47:18 +0600

    light refactoring

--- server/src/main/kotlin/edu/myrza/archke/Main.kt
+++ server/src/main/kotlin/edu/myrza/archke/Main.kt
@@ -1,8 +1,10 @@
 package edu.myrza.archke
 
 import edu.myrza.archke.server.Archke
+import edu.myrza.archke.server.config.ArchkeConfig
 
 fun main(args: Array<String>) {
-    val archke = Archke(9999)
+    val config = ArchkeConfig(port = 9999)
+    val archke = Archke(config)
     archke.start()
 }
--- server/src/main/kotlin/edu/myrza/archke/server/Archke.kt
+++ server/src/main/kotlin/edu/myrza/archke/server/Archke.kt
@@ -1,26 +1,29 @@
 package edu.myrza.archke.server
 
 import edu.myrza.archke.server.command.*
+import edu.myrza.archke.server.config.ArchkeConfig
 import edu.myrza.archke.server.controller.ControllerImpl
 import edu.myrza.archke.server.io.Acceptor
 import edu.myrza.archke.server.io.Reactor
 import edu.myrza.archke.server.db.KeyValueStorageImpl
-import java.io.IOException
 import java.net.InetSocketAddress
 import java.nio.channels.SelectionKey
 import java.nio.channels.Selector
 import java.nio.channels.ServerSocketChannel
 
-class Archke(private val port: Int) {
-
-    private lateinit var reactor: Reactor
+class Archke(private val archkeConfig: ArchkeConfig) {
 
     fun start() {
-        assemble()
-        run()
+        try {
+            val reactor = assemble()
+            run(reactor)
+        } catch (ex: Exception) {
+            println("Error during server start up : ${ex.message}")
+            return
+        }
     }
 
-    private fun assemble() {
+    private fun assemble(): Reactor {
         // SERVICE LAYER
         val service = KeyValueStorageImpl()
 
@@ -36,28 +39,23 @@ class Archke(private val port: Int) {
         // IO LAYER
         val selector = Selector.open()
 
-        val serverChannel: ServerSocketChannel?
-        try {
-            serverChannel = ServerSocketChannel.open().apply {
-                socket().bind(InetSocketAddress(port))
-                configureBlocking(false)
-            }
-        } catch (ex: IOException) {
-            println("Error during server start up : ${ex.message}")
-            return
+        val serverChannel = ServerSocketChannel.open().apply {
+            socket().bind(InetSocketAddress(archkeConfig.port))
+            configureBlocking(false)
         }
-        val key = serverChannel.register(selector, SelectionKey.OP_ACCEPT)
-        val server = Server(selector,serverChannel,controller)
+        val server = Server(selector, serverChannel, controller)
         val acceptor = Acceptor(server)
-        key.attach(acceptor)
 
-        reactor = Reactor(server)
+        val key = serverChannel.register(selector, SelectionKey.OP_ACCEPT)
+        key.attach(acceptor)
 
         // ADDITIONAL COMMANDS
         ShutdownCommand(server).apply { commands[command()] = this }
+
+        return Reactor(server)
     }
 
-    private fun run() {
+    private fun run(reactor: Reactor) {
         val thread = Thread(reactor)
         thread.start()
     }
